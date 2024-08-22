8ebff8f731f3bb78f2fecda5216bff1af6cc7cf2
flavin-jr <fjcvj@cin.ufpe.br>
2024-08-21 22:27:29 -0300

    chore: configure color palette and body initial css

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,21 @@ export default {
     extend: {
       fontFamily: {
         sans: 'Red Hat Text'
+      },
+      backgroundImage: {
+        'first-1': "url('assets/images/image-baklava-desktop.jpg')"
+      },
+      colors: {
+        'der': '#C73B0F',
+        'esor': {
+          50: '#FCF8F6',
+          100: '#F5EEEC',
+          300: '#CAAFA7',
+          400: '#AD8A85',
+          500: '#87635A',
+          900: '#260F08',
+        },
+        'neerg': '#1EA575'
       }
     },
   },
