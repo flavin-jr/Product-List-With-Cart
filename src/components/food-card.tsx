5925834010a7b73df40edfedec77e31958e7c7cd
flavin-jr <fjcvj@cin.ufpe.br>
2024-08-24 17:37:34 -0300

    feat: add logic for delete food from delete button and change icons setup

--- src/components/food-card.tsx
+++ src/components/food-card.tsx
@@ -1,4 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import AddToCartIcon from '../../public/assets/images/icon-add-to-cart.svg';
+import IncrementQuantityIcon from '../../public/assets/images/icon-increment-quantity.svg';
+import DecrementQuantityIcon from '../../public/assets/images/icon-decrement-quantity.svg';
+
 interface FoodCartDisplay {
     name: string;
     price: number;
@@ -17,11 +21,12 @@ interface FoodCardProps {
 export function FoodCard(props: FoodCardProps) {
 
 
-
     const [isFoodAddedToCart, setIsFoodAddedToCart] = useState(false);
     const [foodQuantity, setFoodQuantity] = useState(1);
     function handleFoodAddedToCart() {
-        setIsFoodAddedToCart(true)
+        setIsFoodAddedToCart(true);
+        props.addToCart({ name: props.name, price: props.price, quantity: foodQuantity })
+
     }
     function handleDecrementFood() {
         if (foodQuantity === 1) {
@@ -39,29 +44,38 @@ export function FoodCard(props: FoodCardProps) {
         setFoodQuantity(foodQuantity + 1)
         props.addToCart({ name: props.name, price: props.price, quantity: foodQuantity + 1 })
     }
-
+    useEffect(() => {
+        const index = props.cartList.findIndex((item: FoodCartDisplay) => {
+            return item.name === props.name;
+        })
+        if (index === -1) {
+            setIsFoodAddedToCart(false);
+            setFoodQuantity(1);
+        }
+    }, props.cartList)
     return (
         <div className="flex flex-col  gap-4">
             <div className="flex flex-col items-center">
                 <img className="h-60 rounded-lg" src={props.image} alt="" />
 
                 {isFoodAddedToCart ? (
-                    <div className="flex items-center justify-between px-3 w-40 rounded-full bg-der py-3 -mt-[22px] text-white border border-transparent">
+                    <div className="flex items-center justify-between px-3 w-40 rounded-full bg-der py-3 -mt-[22px] text-white border border-transparent ">
                         <button onClick={handleDecrementFood} className="border border-white rounded-full size-5 flex items-center justify-center">
-                            <img className="size-3" src="public/assets/images/icon-decrement-quantity.svg" alt="" />
+                            <img className="size-3" src={DecrementQuantityIcon} alt="increment icon" />
+
 
                         </button>
                         {foodQuantity}
-                        <button onClick={handleIncrementFood} className=" border border-white rounded-full size-5 flex items-center justify-center">
-                            <img className="size-3 " src="public/assets/images/icon-increment-quantity.svg" alt="" />
+                        <button onClick={handleIncrementFood} className=" border border-white rounded-full size-5 flex items-center justify-center ">
+                            <img className="size-3" src={IncrementQuantityIcon} alt="increment icon" />
 
                         </button>
 
                     </div>
 
                 ) : (
-                    <button onClick={() => { setIsFoodAddedToCart(true); props.addToCart({ name: props.name, price: props.price, quantity: foodQuantity }) }} className="flex items-center justify-center w-40 rounded-full bg-white border border-esor-400 py-3 -mt-[22px]">
-                        <img src="public/assets/images/icon-add-to-cart.svg" alt="" />
+                    <button onClick={handleFoodAddedToCart} className="flex items-center justify-center gap-2 w-40 rounded-full bg-white border border-esor-400 py-3 -mt-[22px] hover:text-der hover:border-der">
+                        <img src={AddToCartIcon} alt="" />
                         Add to Cart
 
                     </button>
@@ -72,6 +86,8 @@ export function FoodCard(props: FoodCardProps) {
                 <p className="font-semibold text-esor-900">{props.name}</p>
                 <p className="font-semibold text-der">{`$${props.price.toFixed(2)}`}</p>
             </div>
+
         </div>
+
     )
 }
